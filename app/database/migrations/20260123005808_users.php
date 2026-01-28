43d55236192a5ac5f2df8f7038492f3b7495d63c
Paulo Andrade de Oliveira <ryanvasques77@gmail.com>
2026-01-28 18:05:08 -0400

    dfsdfsdf

--- app/database/migrations/20260123005808_users.php
+++ app/database/migrations/20260123005808_users.php
@@ -8,20 +8,17 @@ final class Users extends AbstractMigration
 {
     public function change(): void
     {
-         $table = $this->table('users', ['id' => false, 'primary_key' => ['id']]);
+        $table = $this->table('users', ['id' => false, 'primary_key' => ['id']]);
         $table->addColumn('id', 'biginteger', ['identity' => true, 'null' => false])
             ->addColumn('nome', 'text', ['null' => true])
-<<<<<<< HEAD
             ->addColumn('senha', 'text', ['null' => true])
             ->addColumn('salario', 'decimal', ['null' => true, 'default' => 0, 'precision' => 18, 'scale' => 4])
-=======
             ->addColumn('sobrenome', 'text', ['null' => true])
             ->addColumn('rg', 'text', ['null' => true])
             ->addColumn('cpf', 'text', ['null' => true])
             ->addColumn('senha', 'text', ['null' => true])
             ->addColumn('ativo', 'boolean', ['null' => true])
             ->addColumn('administrador', 'boolean', ['null' => true])
->>>>>>> 53dde170222d35095149a6dd36f7b6ad28de7b56
             ->addColumn('data_cadastro', 'datetime', ['null' => true, 'default' => 'CURRENT_TIMESTAMP'])
             ->addColumn('data_atualizacao', 'datetime', ['null' => true, 'default' => 'CURRENT_TIMESTAMP'])
             ->create();

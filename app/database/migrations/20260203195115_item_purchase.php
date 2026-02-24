952f6fa7412168d99f8ac123e8c6ebfe8d3fca19
Paulo Andrade de Oliveira <ryanvasques77@gmail.com>
2026-02-23 21:48:19 -0400

    neymarrrr

--- app/database/migrations/20260203195115_item_purchase.php
+++ app/database/migrations/20260203195115_item_purchase.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 use Phinx\Migration\AbstractMigration;
 
-final class ItemSale extends AbstractMigration
+final class ItemPurchase extends AbstractMigration
 {
+
     public function change(): void
     {
-        $table = $this->table('item_sale', ['id' => false, 'primary_key' => ['id']]);
+        $table = $this->table('item_purchase', ['id' => false, 'primary_key' => ['id']]);
         $table->addColumn('id', 'biginteger', ['identity' => true, 'null' => false])
-            ->addColumn('id_venda', 'biginteger', ['null' => true])
+            ->addColumn('id_compra', 'biginteger', ['null' => true])
             ->addColumn('id_produto', 'biginteger', ['null' => true])
             ->addColumn('quantidade', 'decimal', ['precision' => 18, 'scale' => 4, 'null' => true])
             ->addColumn('total_bruto', 'decimal', ['precision' => 18, 'scale' => 4, 'null' => true])
@@ -25,7 +26,7 @@ final class ItemSale extends AbstractMigration
             ->addColumn('nome', 'text', ['null' => true])
             ->addColumn('data_cadastro', 'datetime', ['null' => true, 'default' => 'CURRENT_TIMESTAMP'])
             ->addColumn('data_atualizacao', 'datetime', ['null' => true, 'default' => 'CURRENT_TIMESTAMP'])
-            ->addForeignKey('id_venda', 'sale', 'id', ['delete' => 'CASCADE', 'update' => 'NO ACTION'])
+            ->addForeignKey('id_compra', 'purchase', 'id', ['delete' => 'CASCADE', 'update' => 'NO ACTION'])
             ->addForeignKey('id_produto', 'product', 'id', ['delete' => 'CASCADE', 'update' => 'NO ACTION'])
             ->create();
     }
